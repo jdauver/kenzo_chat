b0bc4a0fe78fec438a660be6d02c0e2bfc1bd675
jdauver <71541345+jdauver@users.noreply.github.com>
2020-09-23 10:16:30 +0900

    ｓ

--- kenzo/login.php
+++ kenzo/login.php
@@ -3,8 +3,8 @@ session_start();
 
 if (isset($_POST["hidden"])) {
     try {
-        // $db = new PDO('mysql:host=localhost; dbname=kenzo_chat', 'root', '1234');
-        $db = new PDO('mysql:host=localhost; dbname=kenzo_chat', 'root', 'root');
+        $db = new PDO('mysql:host=localhost; dbname=kenzo_chat', 'root', '1234');
+        // $db = new PDO('mysql:host=localhost; dbname=kenzo_chat', 'root', 'root');
         // $db = new PDO('mysql:host=127.0.0.1; dbname=kenzo_chat', 'root');
         // $db = new PDO('mysql:host=mysql1.php.xdomain.ne.jp; dbname=jdauver_kenzo', 'jdauver_kawa', 'jannedolls1227');
 
